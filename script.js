376e7bc959bfae546f14bf04fe521391aaaf7ef0
vischio <rns11aiwbk@libero.it>
2021-11-15 14:44:38 +0100

    add local storage of books

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const SAVED_LIBRARY = "myLibrary";
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -15,25 +17,36 @@ Book.prototype.toggleRead = function(){
 
 let myLibrary = [];
 
-const book_1 = new Book("I Promessi Sposi", "A. Manzoni", 500, true);
-addCard(book_1);myLibrary.push(book_1);
-const book_2 = new Book("Oliver Twist", "C. Dickens", 480, false);
-addCard(book_2);myLibrary.push(book_2);
-const book_3 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-addCard(book_3);myLibrary.push(book_3);
+let booksSaved = loadLibrary();
+if(booksSaved.length > 0) {
+  myLibrary = myLibrary.concat(booksSaved);
+  displayBook();
+}
 
-function addBookToLibrary() {
+
+if(myLibrary.length === 0)  {
+  const book_1 = new Book("I Promessi Sposi", "A. Manzoni", 500, true);
+  addCard(myLibrary.length, book_1);myLibrary.push(book_1); 
+  const book_2 = new Book("Oliver Twist", "C. Dickens", 480, false);
+  addCard(myLibrary.length, book_2);myLibrary.push(book_2);
+  const book_3 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+  addCard(myLibrary.length, book_3);myLibrary.push(book_3);
+}
+
+saveLibrary(myLibrary);
+
+/* function addBookToLibrary() {
   let bTitle = prompt("Book title?");
   let bAuthor = prompt("Book Author?");
   let bPages = prompt("Book pages?");
   let bRead = prompt("Alredy read?");
 
   myLibrary.push(new Book(bTitle, bAuthor, bPages, bRead));
-}
+} */
 
 function displayBook() {
-  for(let book of myLibrary) {
-    addCard(book);
+  for(const [index, book] of myLibrary.entries()) {
+    addCard(index, book);
   }
 }
 
@@ -54,16 +67,16 @@ let button_enter = document.querySelector("button.enter");
   
   let new_book = new Book(title, author, pages, read);
   
-  addCard(new_book);
+  addCard(myLibrary.length ,new_book);
   myLibrary.push(new_book);
-
+  saveLibrary(myLibrary);
 });
 
-function addCard(book) {
+function addCard(index, book) {
   let container = document.querySelector("div.book_container");
   let card = document.createElement("div");
   card.className = "card";
-  card.setAttribute("data-index", `${myLibrary.length}`);
+  card.setAttribute("data-index", `${index}`);
   let read_paragraph = document.createElement("p");
   read_paragraph.innerText = `${book.read ? "already read": "not read yet"}`
   card.innerHTML =`<h2>${book.title}</h2><p>Author: ${book.author}</p><p>${book.pages} pages</p>`;
@@ -74,9 +87,11 @@ function addCard(book) {
     toggleRead.className="toggleRead";
     toggleRead.innerText="Ok,I read it";
     card.appendChild(toggleRead);
-    toggleRead.addEventListener("click", function() {
+    toggleRead.addEventListener("click", function() { 
       myLibrary[card.dataset.index].toggleRead();
       read_paragraph.innerText = "already read";
+      card.removeChild(toggleRead);
+      saveLibrary(myLibrary);
     });
   }
 
@@ -87,7 +102,25 @@ function addCard(book) {
   remove.addEventListener("click", function() {
       container.removeChild(card);
       myLibrary[card.dataset.index] = null;
+      saveLibrary(myLibrary);
   } );
-   
+}
 
+function saveLibrary(library) {
+  localStorage.setItem(SAVED_LIBRARY, JSON.stringify(library));
+}
+
+function loadLibrary() {
+
+  let books = [];
+  let lib = localStorage.getItem(SAVED_LIBRARY); 
+  if(!lib) return [];
+  lib = JSON.parse(lib);
+  for(let obj of lib) {
+    if(obj === null) continue;
+    let book = new Book(obj.title, obj.author, obj.pages, obj.read);
+    books.push(book);
+  }
+
+  return books;
 }
